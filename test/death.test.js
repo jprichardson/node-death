a9b8fb1ca554b626d04fe71d6f827c16c4668420
JP Richardson <jprichardson@gmail.com>
2013-02-18 13:33:47 -0600

    Implemented tests for defaults.

--- test/death.test.js
+++ test/death.test.js
@@ -1,2 +1,39 @@
-var exec = require('child_process').exec
+var spawn = require('child_process').spawn //require('win-spawn')
+  , P = require('autoresolve')
+  , testutil = require('testutil')
+  , colors = require('colors')
 
+describe('death', function() {
+  describe('default behavior', function() {
+    it('should catch SIGINT, SIGTERM, and SIGQUIT and return 3', function(done) {
+      var signals = []
+      var progPath = P('test/resources/default')
+      var prog = spawn(progPath, [])
+      //console.dir(prog)
+
+      prog.stdout.on('data', function(data) {
+        //console.log(colors.cyan(data.toString()))
+      })
+
+      prog.stderr.on('data', function(data) {
+        //console.error(colors.red(data.toString()))
+        signals.push(data.toString().trim())
+      })
+
+      prog.on('exit', function(code) {
+        EQ (code, 3)
+        T (signals.indexOf('SIGQUIT') >= 0)
+        T (signals.indexOf('SIGTERM') >= 0)
+        T (signals.indexOf('SIGINT') >= 0)
+        done()
+      })
+
+      setTimeout(function() {
+        prog.kill('SIGINT')
+        process.kill(prog.pid, 'SIGTERM')
+        prog.kill('SIGQUIT')
+      }, 100)
+      
+    })
+  })
+})
